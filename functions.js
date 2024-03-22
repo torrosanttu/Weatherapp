633242a17a307e48d3296fb284712fa23479e7f0
torrosanttu <santtu.torro@gmail.com>
2024-03-22 09:02:09 +0200

    js- koodia korjattu, nyt näkyy tuulenkin tiedot

--- functions.js
+++ functions.js
@@ -31,6 +31,8 @@ const getWeather = (lat,lng) => {
         .then(response => {
             const json = response.data
             temp_span.innerHTML = json.main.temp + '&#8451;'
+            speed_span.innerHTML = json.wind.speed + ' m/s'
+            direction_span.innerHTML = json.wind.deg + '&#176;'
         }).catch(error => {
             alert(error)
         })
@@ -39,9 +41,10 @@ const getWeather = (lat,lng) => {
 getLocation()
 
 then(response => {
-    const json = response.datatemp_span.innerHTML = json.main.temp + '&8451;'
-    speed_span.innerHTML = json-wind.speed + ' m/s'
-    direction_span.innerHTML = json-wind.deg + '&#176;'
+    const json = response.data
+    temp_span.innerHTML = json.main.temp + '&8451;'
+    speed_span.innerHTML = json.wind.speed + ' m/s'
+    direction_span.innerHTML = json.wind.deg + '&#176;'
     description_span.innerHTML = json.weather[0].description
     const image = icon_url + json.weather[0].icon + '@2x.png'
     icon_img.src = image
