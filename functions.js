10ac5f527c5580595645b7aece70a0894aba3779
torrosanttu <santtu.torro@gmail.com>
2024-03-22 09:05:29 +0200

    JS- koodissa siirretty kaikki nyt oikeaan paikkaan

--- functions.js
+++ functions.js
@@ -33,19 +33,12 @@ const getWeather = (lat,lng) => {
             temp_span.innerHTML = json.main.temp + '&#8451;'
             speed_span.innerHTML = json.wind.speed + ' m/s'
             direction_span.innerHTML = json.wind.deg + '&#176;'
+            description_span.innerHTML = json.weather[0].description
+            const image = icon_url + json.weather[0].icon + '@2x.png'
+            icon_img.src = image
         }).catch(error => {
             alert(error)
         })
 }
 
 getLocation()
-
-then(response => {
-    const json = response.data
-    temp_span.innerHTML = json.main.temp + '&8451;'
-    speed_span.innerHTML = json.wind.speed + ' m/s'
-    direction_span.innerHTML = json.wind.deg + '&#176;'
-    description_span.innerHTML = json.weather[0].description
-    const image = icon_url + json.weather[0].icon + '@2x.png'
-    icon_img.src = image
-})
